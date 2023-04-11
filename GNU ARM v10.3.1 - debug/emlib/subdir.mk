0e04db905e366052fa6f6930219e589d96b20923
timS <lbdl_kb@protonmail.com>
2023-04-12 00:36:26 +0100

    Add sl timer and clean

--- GNU ARM v10.3.1 - debug/emlib/subdir.mk
+++ GNU ARM v10.3.1 - debug/emlib/subdir.mk
@@ -4,13 +4,14 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-/Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_cmu.c \
-/Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_core.c \
-/Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_emu.c \
-/Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_gpio.c \
-/Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_iadc.c \
-/Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_prs.c \
-/Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_system.c 
+../emlib/em_cmu.c \
+../emlib/em_core.c \
+../emlib/em_emu.c \
+../emlib/em_gpio.c \
+../emlib/em_iadc.c \
+../emlib/em_letimer.c \
+../emlib/em_prs.c \
+../emlib/em_system.c 
 
 OBJS += \
 ./emlib/em_cmu.o \
@@ -18,6 +19,7 @@ OBJS += \
 ./emlib/em_emu.o \
 ./emlib/em_gpio.o \
 ./emlib/em_iadc.o \
+./emlib/em_letimer.o \
 ./emlib/em_prs.o \
 ./emlib/em_system.o 
 
@@ -27,54 +29,62 @@ C_DEPS += \
 ./emlib/em_emu.d \
 ./emlib/em_gpio.d \
 ./emlib/em_iadc.d \
+./emlib/em_letimer.d \
 ./emlib/em_prs.d \
 ./emlib/em_system.d 
 
 
 # Each subdirectory must supply rules for building sources it contributes
-emlib/em_cmu.o: /Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_cmu.c emlib/subdir.mk
+emlib/em_cmu.o: ../emlib/em_cmu.c emlib/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C Compiler'
 	arm-none-eabi-gcc -g3 -gdwarf-2 -mcpu=cortex-m33 -mthumb -std=c99 '-DEFR32MG22C224F512IM40=1' -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/EFR32MG22_BRD4182A/config" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/CMSIS/Core/Include" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/emlib/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/bsp" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/drivers" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/Device/SiliconLabs/EFR32MG22/Include" -O0 -Wall -ffunction-sections -fdata-sections -mfpu=fpv5-sp-d16 -mfloat-abi=softfp -c -fmessage-length=0 -mcmse -MMD -MP -MF"emlib/em_cmu.d" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-emlib/em_core.o: /Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_core.c emlib/subdir.mk
+emlib/em_core.o: ../emlib/em_core.c emlib/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C Compiler'
 	arm-none-eabi-gcc -g3 -gdwarf-2 -mcpu=cortex-m33 -mthumb -std=c99 '-DEFR32MG22C224F512IM40=1' -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/EFR32MG22_BRD4182A/config" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/CMSIS/Core/Include" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/emlib/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/bsp" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/drivers" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/Device/SiliconLabs/EFR32MG22/Include" -O0 -Wall -ffunction-sections -fdata-sections -mfpu=fpv5-sp-d16 -mfloat-abi=softfp -c -fmessage-length=0 -mcmse -MMD -MP -MF"emlib/em_core.d" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-emlib/em_emu.o: /Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_emu.c emlib/subdir.mk
+emlib/em_emu.o: ../emlib/em_emu.c emlib/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C Compiler'
 	arm-none-eabi-gcc -g3 -gdwarf-2 -mcpu=cortex-m33 -mthumb -std=c99 '-DEFR32MG22C224F512IM40=1' -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/EFR32MG22_BRD4182A/config" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/CMSIS/Core/Include" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/emlib/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/bsp" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/drivers" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/Device/SiliconLabs/EFR32MG22/Include" -O0 -Wall -ffunction-sections -fdata-sections -mfpu=fpv5-sp-d16 -mfloat-abi=softfp -c -fmessage-length=0 -mcmse -MMD -MP -MF"emlib/em_emu.d" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-emlib/em_gpio.o: /Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_gpio.c emlib/subdir.mk
+emlib/em_gpio.o: ../emlib/em_gpio.c emlib/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C Compiler'
 	arm-none-eabi-gcc -g3 -gdwarf-2 -mcpu=cortex-m33 -mthumb -std=c99 '-DEFR32MG22C224F512IM40=1' -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/EFR32MG22_BRD4182A/config" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/CMSIS/Core/Include" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/emlib/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/bsp" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/drivers" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/Device/SiliconLabs/EFR32MG22/Include" -O0 -Wall -ffunction-sections -fdata-sections -mfpu=fpv5-sp-d16 -mfloat-abi=softfp -c -fmessage-length=0 -mcmse -MMD -MP -MF"emlib/em_gpio.d" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-emlib/em_iadc.o: /Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_iadc.c emlib/subdir.mk
+emlib/em_iadc.o: ../emlib/em_iadc.c emlib/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C Compiler'
 	arm-none-eabi-gcc -g3 -gdwarf-2 -mcpu=cortex-m33 -mthumb -std=c99 '-DEFR32MG22C224F512IM40=1' -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/EFR32MG22_BRD4182A/config" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/CMSIS/Core/Include" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/emlib/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/bsp" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/drivers" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/Device/SiliconLabs/EFR32MG22/Include" -O0 -Wall -ffunction-sections -fdata-sections -mfpu=fpv5-sp-d16 -mfloat-abi=softfp -c -fmessage-length=0 -mcmse -MMD -MP -MF"emlib/em_iadc.d" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-emlib/em_prs.o: /Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_prs.c emlib/subdir.mk
+emlib/em_letimer.o: ../emlib/em_letimer.c emlib/subdir.mk
+	@echo 'Building file: $<'
+	@echo 'Invoking: GNU ARM C Compiler'
+	arm-none-eabi-gcc -g3 -gdwarf-2 -mcpu=cortex-m33 -mthumb -std=c99 '-DEFR32MG22C224F512IM40=1' -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/EFR32MG22_BRD4182A/config" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/CMSIS/Core/Include" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/emlib/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/bsp" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/drivers" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/Device/SiliconLabs/EFR32MG22/Include" -O0 -Wall -ffunction-sections -fdata-sections -mfpu=fpv5-sp-d16 -mfloat-abi=softfp -c -fmessage-length=0 -mcmse -MMD -MP -MF"emlib/em_letimer.d" -MT"$@" -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+emlib/em_prs.o: ../emlib/em_prs.c emlib/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C Compiler'
 	arm-none-eabi-gcc -g3 -gdwarf-2 -mcpu=cortex-m33 -mthumb -std=c99 '-DEFR32MG22C224F512IM40=1' -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/EFR32MG22_BRD4182A/config" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/CMSIS/Core/Include" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/emlib/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/bsp" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/drivers" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/Device/SiliconLabs/EFR32MG22/Include" -O0 -Wall -ffunction-sections -fdata-sections -mfpu=fpv5-sp-d16 -mfloat-abi=softfp -c -fmessage-length=0 -mcmse -MMD -MP -MF"emlib/em_prs.d" -MT"$@" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
-emlib/em_system.o: /Users/tims/SimplicityStudio/SDKs/gecko_sdk/platform/emlib/src/em_system.c emlib/subdir.mk
+emlib/em_system.o: ../emlib/em_system.c emlib/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C Compiler'
 	arm-none-eabi-gcc -g3 -gdwarf-2 -mcpu=cortex-m33 -mthumb -std=c99 '-DEFR32MG22C224F512IM40=1' -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/EFR32MG22_BRD4182A/config" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/CMSIS/Core/Include" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/emlib/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/bsp" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/drivers" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/Device/SiliconLabs/EFR32MG22/Include" -O0 -Wall -ffunction-sections -fdata-sections -mfpu=fpv5-sp-d16 -mfloat-abi=softfp -c -fmessage-length=0 -mcmse -MMD -MP -MF"emlib/em_system.d" -MT"$@" -o "$@" "$<"
--- GNU ARM v10.3.1 - debug/src/subdir.mk
+++ GNU ARM v10.3.1 - debug/src/subdir.mk
@@ -4,7 +4,7 @@
 
 # Add inputs and outputs from these tool invocations to the build variables 
 C_SRCS += \
-/Users/tims/DATA/HX/ex/peripheral_examples/series2/iadc/iadc_single_oversampling/src/main_single_oversampling_16bit.c 
+../src/main_single_oversampling_16bit.c 
 
 OBJS += \
 ./src/main_single_oversampling_16bit.o 
@@ -14,7 +14,7 @@ C_DEPS += \
 
 
 # Each subdirectory must supply rules for building sources it contributes
-src/main_single_oversampling_16bit.o: /Users/tims/DATA/HX/ex/peripheral_examples/series2/iadc/iadc_single_oversampling/src/main_single_oversampling_16bit.c src/subdir.mk
+src/main_single_oversampling_16bit.o: ../src/main_single_oversampling_16bit.c src/subdir.mk
 	@echo 'Building file: $<'
 	@echo 'Invoking: GNU ARM C Compiler'
 	arm-none-eabi-gcc -g3 -gdwarf-2 -mcpu=cortex-m33 -mthumb -std=c99 '-DEFR32MG22C224F512IM40=1' -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/EFR32MG22_BRD4182A/config" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/CMSIS/Core/Include" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/emlib/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/bsp" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/common/inc" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//hardware/kit/common/drivers" -I"/Users/tims/SimplicityStudio/SDKs/gecko_sdk//platform/Device/SiliconLabs/EFR32MG22/Include" -O0 -Wall -ffunction-sections -fdata-sections -mfpu=fpv5-sp-d16 -mfloat-abi=softfp -c -fmessage-length=0 -mcmse -MMD -MP -MF"src/main_single_oversampling_16bit.d" -MT"$@" -o "$@" "$<"
